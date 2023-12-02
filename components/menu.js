cf1ec88ed5ebdbe437ec7e6a5056d12e1f19434b
Jeff <148919566+jeff96655@users.noreply.github.com>
2023-12-02 09:30:50 +0100

    Update menu.js

--- components/menu.js
+++ components/menu.js
@@ -32,7 +32,7 @@ const Menu = ({ onClose }) => {
   }, []);
 
   const onFrameButton2Click = useCallback(() => {
-    window.open("https://blockfronttech.vercel.app");
+    window.open("https://jeffspace.vercel.app");
   }, []);
 
   return (
